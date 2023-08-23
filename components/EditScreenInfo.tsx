1ea56ebc5108f0ec2d4c0b960e8e80e5693752d9
pinky-pig <2545589993@qq.com>
2023-08-23 10:15:34 +0800

    fix: eslint code

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -10,6 +10,36 @@ import { AppName } from '~/constants/Params'
 
 // import '~/styles/global.css'
 
+const styles = StyleSheet.create({
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: 'center',
+  },
+})
+
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
@@ -50,33 +80,3 @@ export default function EditScreenInfo({ path }: { path: string }) {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightContainer: {
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  helpContainer: {
-    marginTop: 15,
-    marginHorizontal: 20,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    textAlign: 'center',
-  },
-})

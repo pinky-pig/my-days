161dfadfbf6384aad9bb58a43d14f57069e6229c
pinky-pig <2545589993@qq.com>
2023-08-28 14:38:58 +0800

    fix: npx expo install --fix

--- app/pic-map/index.tsx
+++ app/pic-map/index.tsx
@@ -1,12 +1,7 @@
-import 'expo-dev-client'
 import { ArrowLeft } from '@tamagui/lucide-icons'
 import { Stack, useRouter, useSearchParams } from 'expo-router'
 import { Button, H3, H6, View, XStack, YStack } from 'tamagui'
 import tw from 'twrnc'
-
-// import Mapbox from '@rnmapbox/maps'
-
-// Mapbox.setAccessToken('pk.eyJ1IjoicGlua3ktcGlnIiwiYSI6ImNsZnJuMTQxazAwMmUzb256dW9teG8wa2kifQ.H_xJVll_ljzZ8JgWrqQpZA')
 
 export default function Settings() {
   const router = useRouter()
